03f63e6a081829eaa9bad251216dfe85db177865
Timothy Ko <timothy.l.ko@gmail.com>
2017-12-08 13:50:09 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var Alexa = require('alexa-sdk');
+
+exports.handler = function(event, context, callback){
+    var alexa = Alexa.handler(event, context, callback);
+};
+
+var handlers = {
+    'HelloWorldIntent': function () {
+        this.emit(':tell', 'Hello World!');
+    }
+};
